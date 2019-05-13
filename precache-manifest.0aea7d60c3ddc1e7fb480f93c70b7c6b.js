32a76797ec174cfa53b8cddf0f61835cadfe4ce2
Berkay Barlas <fpberkay@gmail.com>
2019-05-13 04:02:33 +0300

    Updates

---
+++ precache-manifest.0aea7d60c3ddc1e7fb480f93c70b7c6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecffffae3da042a8043b",
+    "url": "/static/css/main.ac29d32d.chunk.css"
+  },
+  {
+    "revision": "ecffffae3da042a8043b",
+    "url": "/static/js/main.add613c8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c15e6746284a03436ea6",
+    "url": "/static/js/2.989a2cff.chunk.js"
+  },
+  {
+    "revision": "e661b3cef9b809c61b982ecc53a3c93a",
+    "url": "/static/media/map.e661b3ce.jpg"
+  },
+  {
+    "revision": "96a9039f177b776e78faa41d7f375dd4",
+    "url": "/static/media/book.96a9039f.svg"
+  },
+  {
+    "revision": "1907da0f6e633e123138fe964f1d007e",
+    "url": "/static/media/books.1907da0f.svg"
+  },
+  {
+    "revision": "aa0143301dbff56df5596be7d6721cf9",
+    "url": "/static/media/map.aa014330.svg"
+  },
+  {
+    "revision": "cae73e3808177615f6e87c4208119646",
+    "url": "/static/media/logo.cae73e38.png"
+  },
+  {
+    "revision": "405b656c0aac2411159747dfcfb2bcb0",
+    "url": "/index.html"
+  }
+];
